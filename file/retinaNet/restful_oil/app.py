4eb882ad25debc5eb8f5ab698fc01be628c92279
zhangjing1 <zhagnjing1@mail.e-u.cn>
2018-06-27 13:23:27 +0800

    new

--- file/retinaNet/restful_oil/app.py
+++ file/retinaNet/restful_oil/app.py
@@ -1,4 +1,7 @@
 #coding=utf-8
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from model import Model
 from flask import request,Response
@@ -25,11 +28,26 @@ mm = Model(cfg_path,weights_path)
 
 @app.route('/user', methods=['POST'])
 def info():
-
+    #app.logger.warning('A warning occurred (%d apples)', 42)
+    #app.logger.error('An error occurred')                     #private log
+    #logging.info('Info')                                      #root log
+    
+    
     #imagepath = request.form.getlist('data')
     #imagepath = request.form.get("data",type=str,default=None)
     js = request.get_json()
-    imagepath = js.get('data',None)
+    
+    #return json init
+    out_json = {"data":[]}
+    
+    #js is not dict return []
+    if isinstance(js,dict) and js.has_key('data') :
+        imagepath = js.get('data',None)
+        info_str = 'images path:' + imagepath
+        logger.info(info_str)
+    else:
+        logger.warning('post has not data or data-key!!!')
+        return json.dumps(out_json)
     if (imagepath.startswith("https://") or imagepath.startswith("http://") or imagepath.startswith("file://")):
         imagefile = urllib.urlopen(imagepath)
         status=imagefile.code
@@ -41,25 +59,50 @@ def info():
             new_imagepath = image_name
             with open(new_imagepath, 'wb') as code:
                 code.write(image_data)
-            img_np = misc.imread(new_imagepath)
+            #img_np = misc.imread(new_imagepath)
+            img_np = cv2.imread(new_imagepath)  #read image by cv2 ,the same as /tool/test_net.py
         else:
-            out_json = {"data":[]}
+            logger.warning('the image is not download on internet!!!')
             return json.dumps(out_json)
     # path 
     else:
         if not os.path.exists(imagepath):
-            out_json = {"data":[]}
+            logger.warning('the image is not exists!!!')
             return json.dumps(out_json)
         else:
-            img_np = misc.imread(imagepath)
-    return mm.predict(img_np)
-    
+            #img_np = misc.imread(imagepath)
+            img_np = cv2.imread(imagepath)  #read image by cv2 ,the same as /tool/test_net.py
+            
+   
+    predict_datalist = mm.predict(img_np)
+    if len(predict_datalist) > 0:
+        logger.info('the images predict completed!!!')
+        res_log = []
+        for i in range(len(predict_datalist)):
+            single_data = {}
+            single_data = predict_datalist[i]
+            res_log.append(single_data['cls'])
+        logger.info(res_log)
+        out_json["data"] = predict_datalist
+    else:
+        logger.warning('the images has not right bbox!!!')
+    return json.dumps(out_json)
     
     
 if __name__ == '__main__':
+
+    if not os.path.exists('./log'):
+        os.makedirs('./log')
     
-    app.run(host="0.0.0.0",port=8080,debug=False)
+    logger = logging.getLogger('')    #set root level , default is WRAINING
+    logger.setLevel(logging.DEBUG)
     
-    
-#,threaded=True
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s - %(module)s - %(funcName)s:%(lineno)d} - %(message)s")
+    handler = RotatingFileHandler('./log/oilsteal.log', maxBytes=10000000, backupCount=10)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)     #ok  start root log
+    #app.logger.addHandler(handler)  #ok  start private log
+    app.run(host="0.0.0.0",port=8080,debug=False)   #threaded=True
+
     
--- file/retinaNet/restful_oil/model.py
+++ file/retinaNet/restful_oil/model.py
@@ -1,5 +1,4 @@
 #coding=utf-8
-
 import numpy as np
 import cv2 
 import json
@@ -16,20 +15,37 @@ c2_utils.import_detectron_ops()
 # thread safe and causes unwanted GPU memory allocations.
 cv2.ocl.setUseOpenCL(False)
 
-# Restful服务，可参考tools/ infer_simple.py来写调用过程。
-
 class Model():
     
     def __init__(self,cfg_path,weights_path):
-        self.gpu_id = 0
-        self.thresh = 0.3
+    
+        #nms_same_class 0.3  ----  *.yaml/TEST:NMS:0.3 中设置 defalut 0.5
+        
+        self.gpu_id = 0               #gpu_id default 0
+        
+        self.score_thresh = 0.4       #score > score_thresh  default 0.3  
+        
+        self.per_class_thresh = False         #score > class_score_thresh
+        self.autotruck_score_thresh = 0.6
+        self.forklift_score_thresh = 0.65
+        self.digger_score_thresh = 0.65
+        self.car_score_thresh = 0.45
+        self.bus_score_thresh = 0.0
+        self.tanker_score_thresh = 0.55
+        self.person_score_thresh = 0.35
+        self.minitruck_score_thresh = 0.0
+        self.minibus_score_thresh = 0.59
+        
+        self.class_nms_thresh = 0.85   #nms_between_classes  IOU > class_nms_thresh    default 0.9 
         merge_cfg_from_file(cfg_path)
         self.model = infer_engine.initialize_model_from_cfg(weights_path,self.gpu_id)
-        self.dummy_coco_dataset = dummy_datasets.get_steal_oil_class8_dataset()	#类别名称
+        self.dummy_coco_dataset = dummy_datasets.get_steal_oil_class10_dataset()
         print ("model is ok")
 
     def predict(self,im):
-        out_json = {"data":[]}
+
+        #class_str_list = []
+        data_list = []
         
         with c2_utils.NamedCudaScope(self.gpu_id):
             cls_boxes, cls_segms, cls_keyps = infer_engine.im_detect_all(self.model, im, None, None
@@ -38,29 +54,81 @@ class Model():
         #get box classes
         if isinstance(cls_boxes, list):
             boxes, segms, keypoints, classes = self.convert_from_cls_format(cls_boxes, cls_segms, cls_keyps)
-        if boxes is None or boxes.shape[0] == 0 or max(boxes[:, 4]) < self.thresh:
-            return json.dumps(out_json)
+        if boxes is None or boxes.shape[0] == 0 or max(boxes[:, 4]) < self.score_thresh:
+            return data_list
         #get score
         areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
         sorted_inds = np.argsort(-areas)
-        #class_str_list = []
-        data_list = []
+        
+        
+        #no nms between classes
+        '''im1 = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
+        result1= im1.copy()
         for i in sorted_inds:
             
             bbox = boxes[i, :4]
             score = boxes[i, -1]
-            if score < self.thresh:
+            if score < self.score_thresh:
                 continue
             #get class-str
-            class_str = self.get_class_string(classes[i], score, self.dummy_coco_dataset)
-            #class_str_list.append(class_str)
-            
-            single_data = {"class":class_str,"score":float('%.2f' % score),"box":{"xmin":int(bbox[0]),"ymin":int(bbox[1]),"xmax":int(bbox[2]),"ymax":int(bbox[3])}}
-            data_list.append(single_data)
-        #construcrion - json
-        out_json["data"] = data_list
+            class_str = self.get_class_string(classes[i], score, self.dummy_coco_dataset)            
+            cv2.rectangle(result1,(int(bbox[0]),int(bbox[1])),(int(bbox[2]),int(bbox[3])),(255,255,0),1)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            ((txt_w, txt_h), _) = cv2.getTextSize(class_str, font, 0.35, 1)            
+            txt_tl = int(bbox[0]), int(bbox[1]) - int(0.3 * txt_h)
+            cv2.putText(result1, class_str, txt_tl, font, 0.35, (218, 227, 218), lineType=cv2.LINE_AA)
+            txt_tl = int(bbox[0])+txt_w, int(bbox[1]) - int(0.3 * txt_h)
+            cv2.putText(result1, ('%.2f' % score), txt_tl, font, 0.35, (218, 227, 218), lineType=cv2.LINE_AA)
+        cv2.imwrite("test1.jpg", result1)'''
         
-        return json.dumps(out_json)
+        #nms between classes
+        #im2 = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
+        #result2= im2.copy()        
+        if (len(sorted_inds) > 0):        
+            nmsIndex = self.nms_between_classes(boxes, self.class_nms_thresh)  #阈值为0.9，阈值越大，过滤的越少 
+            for i in xrange(len(nmsIndex)):                
+                bbox = boxes[nmsIndex[i], :4]
+                score = boxes[nmsIndex[i], -1]
+                if score < self.score_thresh:
+                    continue
+                #get class-str
+                class_str = self.get_class_string(classes[nmsIndex[i]], score, self.dummy_coco_dataset)
+                
+                #score thresd per class
+                if self.per_class_thresh:
+                    if 'autotruck' == class_str and score < self.autotruck_score_thresh:
+                        continue
+                    if 'forklift'  == class_str and score < self.forklift_score_thresh:
+                        continue
+                    if 'digger'    == class_str and score < self.digger_score_thresh:
+                        continue
+                    if 'car'       == class_str and score < self.car_score_thresh:
+                        continue
+                    if 'bus'       == class_str and score < self.bus_score_thresh:
+                        continue
+                    if 'tanker'    == class_str and score < self.tanker_score_thresh:
+                        continue
+                    if 'person'    == class_str and score < self.person_score_thresh:
+                        continue
+                    if 'minitruck' == class_str and score < self.minitruck_score_thresh:
+                        continue
+                    if 'minibus'   == class_str and score < self.minibus_score_thresh:
+                        continue
+                
+                single_data = {"cls":class_str,"score":float('%.2f' % score),"bbox":{"xmin":int(bbox[0]),"ymin":int(bbox[1]),"xmax":int(bbox[2]),"ymax":int(bbox[3])}}
+                data_list.append(single_data)        
+        
+                '''cv2.rectangle(result2,(int(bbox[0]),int(bbox[1])),(int(bbox[2]),int(bbox[3])),(255,255,0),1)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                ((txt_w, txt_h), _) = cv2.getTextSize(class_str, font, 0.55, 1)            
+                txt_tl = int(bbox[0]), int(bbox[1]) - int(0.3 * txt_h)
+                cv2.putText(result2, class_str, txt_tl, font, 0.55, (218, 227, 218), lineType=cv2.LINE_AA)
+                txt_tl = int(bbox[0])+txt_w, int(bbox[1]) - int(0.3 * txt_h)
+                cv2.putText(result2, ('%.2f' % score), txt_tl, font, 0.35, (218, 227, 218), lineType=cv2.LINE_AA)'''
+        #cv2.imwrite("test2.jpg", result2)
+        
+        #construcrion - data_list
+        return data_list
         
     def convert_from_cls_format(self,cls_boxes, cls_segms, cls_keyps):
         """Convert from the class boxes/segms/keyps format generated by the testing
@@ -89,4 +157,33 @@ class Model():
             'id{:d}'.format(class_index)
         #return class_text + ' {:0.2f}'.format(score).lstrip('0')
         return class_text
+    def nms_between_classes(self,boxes, threshold):
+        if boxes.size==0:
+            return np.empty((0,3))
+        x1 = boxes[:,0]
+        y1 = boxes[:,1]
+        x2 = boxes[:,2]
+        y2 = boxes[:,3]
+        s = boxes[:,4]
+        area = (x2-x1+1) * (y2-y1+1)
+        I = np.argsort(s)
+        pick = np.zeros_like(s, dtype=np.int16)
+        counter = 0
+        while I.size>0:
+            i = I[-1]
+            pick[counter] = i
+            counter += 1
+            idx = I[0:-1]
+            xx1 = np.maximum(x1[i], x1[idx])
+            yy1 = np.maximum(y1[i], y1[idx])
+            xx2 = np.minimum(x2[i], x2[idx])
+            yy2 = np.minimum(y2[i], y2[idx])
+            w = np.maximum(0.0, xx2-xx1+1)
+            h = np.maximum(0.0, yy2-yy1+1)
+            inter = w * h        
+            o = inter / (area[i] + area[idx] - inter)
+            I = I[np.where(o<=threshold)]
+        pick = pick[0:counter]  #返回nms后的索引
+        return pick
+    
     
